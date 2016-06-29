ec6a230739d6b3114c8775b716ee23e38c527edb
Alexandre Andrade <kaniabi@gmail.com>
2016-06-29 08:58:39 -0300

    Adding CACHES: pip only for now.

---
+++ caches/install.zsh
@@ -0,0 +1,3 @@
+export PIP_INDEX_URL=http://hack-01.axado.com.br:3141/root/pypi/+simple/
+export PIP_TRUSTED_HOST=hack-01.axado.com.br
+
--- virtualenvwrapper/setup.zsh
+++ virtualenvwrapper/setup.zsh
@@ -3,9 +3,5 @@ export PROJECT_HOME=$HOME/Projects
 source /usr/local/bin/virtualenvwrapper.sh
 
 export GIT_EDITOR=vim
-# We are using fleet now, so DOCKER_HOST no longer works ... keeping for future reference.
-#export DOCKER_HOST=tcp://54.207.132.204:2375
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_PYTHON=python2
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-
-export FLEETCTL_TUNNEL=54.232.124.153:22
