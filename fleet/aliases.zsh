6b257719bce99b7c989e0cb2da0991e72af213ee
Alexandre Andrade <kaniabi@gmail.com>
2016-07-11 15:41:33 -0300

    Adding NPM and BOWER caches configurations.

--- fleet/aliases.zsh
+++ fleet/aliases.zsh
@@ -2,6 +2,6 @@
 alias ff="fleetctl"
 alias ffdir="fleetctl list-units"
 alias ffst="fleetctl status"
-alias ffj="fleetctl journal"
+alias ffj="fleetctl journal -lines 50"
 alias ffdel="fleetctl destroy"
 alias ffstart="fleetctl start"
