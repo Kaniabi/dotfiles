42bcb1bce2a1a0efc4b57e7b764bcab4f8e21c23
Alexandre Andrade <kaniabi@gmail.com>
2017-01-22 20:45:31 +0000

    Disabling cached.

--- caches/install.zsh
+++ caches/install.zsh
@@ -1,11 +1,11 @@
 # cached-pip
-export PIP_TRUSTED_HOST=pip.opexe
-export PIP_INDEX_URL=http://${PIP_TRUSTED_HOST}:3141/root/pypi/+simple/
+# export PIP_TRUSTED_HOST=pip.opexe
+# export PIP_INDEX_URL=http://${PIP_TRUSTED_HOST}:3141/root/pypi/+simple/
 
 # cached-npm
-export NPM_CONFIG_PROXY=http://hack-01:8080/
-export NPM_CONFIG_HTTPS_PROXY=http://hack-01:8080/
-export NPM_CONFIG_STRICT_SSL=false
+# export NPM_CONFIG_PROXY=http://hack-01:8080/
+# export NPM_CONFIG_HTTPS_PROXY=http://hack-01:8080/
+# export NPM_CONFIG_STRICT_SSL=false
 
 # cached-bower
-export BOWER_REGISTRY=http://hack-01:6010
+# export BOWER_REGISTRY=http://hack-01:6010
