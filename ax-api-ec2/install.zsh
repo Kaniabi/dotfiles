64412baf58bab51051b072e42ef3049b8b505661
Alexandre Andrade <kaniabi@gmail.com>
2017-03-21 12:47:56 -0300

    Updating api_log format.

--- ax-api-ec2/install.zsh
+++ ax-api-ec2/install.zsh
@@ -1,4 +1,4 @@
-api_log() {
-    sudo tail -f /var/log/uwsgi/app/api.log | awk '{ print $24 "\t" $17 "\t (" $27 "\t" $18}'
+api_log () {
+  sudo tail -f /var/log/uwsgi/app/api.log | awk '{ print $15 "\t" $24 "\t" $17 "\t (" $27 "\t" $18}'
 }
 
