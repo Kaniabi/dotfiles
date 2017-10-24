e6f3af482ab746297d38c775241eb259f0304d1a
Alexandre Andrade <kaniabi@gmail.com>
2017-10-24 09:53:29 -0200

    Propertly configuring VIRTUALENVWRAPPER_PYTHON for Mac.

--- virtualenvwrapper/setup.zsh
+++ virtualenvwrapper/setup.zsh
@@ -1,5 +1,5 @@
 export GIT_EDITOR=vim
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 export WORKON_HOME=$HOME/VirtualEnvs
