d5a62fca29f65e1587a4c6c3d41aa370c1e62e2b
Alexandre Andrade <kaniabi@gmail.com>
2019-11-11 09:47:19 -0300

    ALIAS: ddbuild instead of ddb. Remove hardcoded home directory.

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -3,10 +3,10 @@
 # alias ddi="sudo docker images"
 # alias ddps="sudo docker ps"
 
-export DEV_DOCKER_DIR=/d/Projects/unhaggle/dev_docker
+export DEV_DOCKER_DIR=/mnt/c/Users/kania/Projects/unhaggle/dev_docker
 
 ddr () { docker-compose -f ${DEV_DOCKER_DIR}/dev_docker/docker-compose.yml run "$@" }
-ddb () { docker-compose -f ${DEV_DOCKER_DIR}/dev_docker/docker-compose.yml build "$@" }
+ddbuild () { docker-compose -f ${DEV_DOCKER_DIR}/dev_docker/docker-compose.yml build "$@" }
 ddup () { docker-compose -f ${DEV_DOCKER_DIR}/dev_docker/docker-compose.yml up "$@" }
 ddstop () { docker-compose -f ${DEV_DOCKER_DIR}/dev_docker/docker-compose.yml stop "$@" }
 ddcog () { cog -r $DEV_DOCKER_DIR/dev_docker/docker-compose.yml }
