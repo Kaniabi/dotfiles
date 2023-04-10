298c98ebc1e2059f1ab9da6107231b6c9b620652
Alexandre Andrade <kaniabi@gmail.com>
2023-04-10 01:49:53 +0000

    Do not install Python.

--- python/install.zsh
+++ python/install.zsh
@@ -4,8 +4,6 @@ if [[ ! -d ~/.pyenv ]]; then
   echo "pyenv: Installing..."
   git clone https://github.com/pyenv/pyenv.git             ~/.pyenv
   git clone https://github.com/pyenv/pyenv-virtualenv.git  $(pyenv root)/plugins/pyenv-virtualenv
-  pyenv install 3.11.3
-  pyenv virtualenv 3.11.3 deen
 fi
 
 function rmpyc () {
@@ -18,4 +16,4 @@ eval "$(pyenv virtualenv-init -)"
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
-pyenv shell deen
+pyenv shell deen | true
