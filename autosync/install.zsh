d443887d2f0297cd1766d11e11cae85bfe8c6654
Alexandre Andrade <kaniabi@gmail.com>
2023-04-10 01:40:58 +0000

    Conditional create AUTOSYNC environment variables.

--- autosync/install.zsh
+++ autosync/install.zsh
@@ -1,17 +1,21 @@
-for i in tier1 tier3 polestar unhaggle bp pi peb; do
-  case $i in
-    bp) REPO_NAME=buildandprice ;;
-    pi) REPO_NAME=paymentinsights ;;
-    *) REPO_NAME=$i ;;
-  esac
-  export ${i:u}_REPO=$HOME/autosync/apps/$REPO_NAME
-  export ${i:u}_DATA=$HOME/autosync/_data
-done
-for i in uh_core moto_core uh_status uh_inventory tierless; do
-  REPO_NAME=$i
-  export ${i:u}_REPO=$HOME/autosync/libs/$REPO_NAME
-  export ${i:u}_DATA=$HOME/autosync/_data
-done
+export AUTOSYNC_DIR=$HOME/autosync
 
-export CAWS_REPO=$HOME/autosync/apps/credit-app
-export CONTAINER_UID=$(id -u)
+if [[ -d $AUTOSYNC_DIR ]]; then
+  for i in tier1 tier3 polestar unhaggle bp pi peb; do
+    case $i in
+      bp) REPO_NAME=buildandprice ;;
+      pi) REPO_NAME=paymentinsights ;;
+      *) REPO_NAME=$i ;;
+    esac
+    export ${i:u}_REPO=$AUTOSYNC_DIR/apps/$REPO_NAME
+    export ${i:u}_DATA=$AUTOSYNC_DIR/_data
+  done
+  for i in uh_core moto_core uh_status uh_inventory tierless; do
+    REPO_NAME=$i
+    export ${i:u}_REPO=$AUTOSYNC_DIR/libs/$REPO_NAME
+    export ${i:u}_DATA=$AUTOSYNC_DIR/_data
+  done
+
+  export CAWS_REPO=$AUTOSYNC_DIR/apps/credit-app
+  export CONTAINER_UID=$(id -u)
+fi
