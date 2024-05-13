f93eba3ff7287f215e557c2a50190c5fcb5d3cd0
Alexandre Andrade <kaniabi@gmail.com>
2024-05-13 14:42:01 +0000

    Re-enabling installation of session-manager-plugin (aws)

--- plugins/aws.zsh
+++ plugins/aws.zsh
@@ -5,7 +5,7 @@ if ( $IS_MAC ); then
 else
   INSTALL_CMD aws "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
   # NOTE: Not working in remote dev.
-  # INSTALL_CMD session-manager-plugin "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb"
+  INSTALL_CMD session-manager-plugin "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb"
   # INSTALL_CMD sam "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip"
   # INSTALL_CMD ecs-cli "https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest"
 fi
