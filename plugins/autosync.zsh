00cc064f0b38d0b992146cfec6270ad93d16e478
Alexandre Andrade <kaniabi@gmail.com>
2024-08-01 19:57:11 +0000

    Fix autosync plugin.

--- plugins/autosync.zsh
+++ plugins/autosync.zsh
@@ -1,5 +1,5 @@
 # 'Autosync stuff'
 
-export TIER3_REPO="~/Code/autosync/tier3"
-export TIER3_DATA="~/Code/autosync/_data"
+export TIER3_REPO="$HOME/Code/autosync/tier3"
+export TIER3_DATA="$HOME/Code/autosync/_data"
 export CONTAINER_UID="$(id -u)"
