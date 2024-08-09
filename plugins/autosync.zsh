427cc392d454d2b13b27081e90d7dbf21709fd2e
Alexandre Andrade <kaniabi@gmail.com>
2024-08-09 13:02:55 +0000

    Add more autosync envs.

--- plugins/autosync.zsh
+++ plugins/autosync.zsh
@@ -1,5 +1,12 @@
 # 'Autosync stuff'
 
+export UNHAGGLE_REPO="$HOME/Code/autosync/unhaggle"
+export UNHAGGLE_DATA="$HOME/Code/autosync/_data"
+
 export TIER3_REPO="$HOME/Code/autosync/tier3"
 export TIER3_DATA="$HOME/Code/autosync/_data"
+
+export UH_INVENTORY_REPO="$HOME/Code/autosync/inventory"
+export UH_INVENTORY_DATA="$HOME/Code/autosync/_data"
+
 export CONTAINER_UID="$(id -u)"
