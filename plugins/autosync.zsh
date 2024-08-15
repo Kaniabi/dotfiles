0b93d74915ae78d3433a457e68eb80a27af68570
Alexandre Andrade <kaniabi@gmail.com>
2024-08-15 12:49:21 +0000

    Working on automatic-shutdown.

--- plugins/autosync.zsh
+++ plugins/autosync.zsh
@@ -6,6 +6,12 @@ export UNHAGGLE_DATA="$HOME/Code/autosync/_data"
 export TIER3_REPO="$HOME/Code/autosync/tier3"
 export TIER3_DATA="$HOME/Code/autosync/_data"
 
+export TIER1_REPO="$HOME/Code/autosync/tier1"
+export TIER1_DATA="$HOME/Code/autosync/_data"
+
+export API_CLIENTS_REPO="$HOME/Code/autosync/api_clients"
+export API_CLIENTS_DATA="$HOME/Code/autosync/_data"
+
 export UH_INVENTORY_REPO="$HOME/Code/autosync/inventory"
 export UH_INVENTORY_DATA="$HOME/Code/autosync/_data"
 
