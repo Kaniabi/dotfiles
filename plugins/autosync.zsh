e91add2d467e0731045ed466fd83ce06f8e40a6a
Alexandre Andrade <kaniabi@gmail.com>
2024-11-26 13:33:07 -0300

    Add more autosync envs.

--- plugins/autosync.zsh
+++ plugins/autosync.zsh
@@ -2,22 +2,31 @@
 
 export DATA_DIR="$HOME/Code/autosync/_data"
 
-export UNHAGGLE_REPO="$HOME/Code/autosync/unhaggle"
+export UNHAGGLE_REPO="$HOME/Code/autosync/apps/unhaggle"
 export UNHAGGLE_DATA="$HOME/Code/autosync/_data"
 
-export TIER3_REPO="$HOME/Code/autosync/tier3"
+export TIER3_REPO="$HOME/Code/autosync/apps/tier3"
 export TIER3_DATA="$HOME/Code/autosync/_data"
 
-export TIER1_REPO="$HOME/Code/autosync/tier1"
+export TIER1_REPO="$HOME/Code/autosync/apps/tier1"
 export TIER1_DATA="$HOME/Code/autosync/_data"
 
-export UH_CORE_REPO="$HOME/Code/autosync/core"
+export UH_CORE_REPO="$HOME/Code/autosync/libs/core"
 export UH_CORE_DATA="$HOME/Code/autosync/_data"
 
-export API_CLIENTS_REPO="$HOME/Code/autosync/api_clients"
+export MOTO_CORE_REPO="$HOME/Code/autosync/libs/moto"
+export MOTO_CORE_DATA="$HOME/Code/autosync/_data"
+
+export UH_STATUS_REPO="$HOME/Code/autosync/libs/status"
+export UH_STATUS_DATA="$HOME/Code/autosync/_data"
+
+export TIERLESS_REPO="$HOME/Code/autosync/libs/tierless"
+export TIERLESS_DATA="$HOME/Code/autosync/_data"
+
+export API_CLIENTS_REPO="$HOME/Code/autosync/libs/api_clients"
 export API_CLIENTS_DATA="$HOME/Code/autosync/_data"
 
-export UH_INVENTORY_REPO="$HOME/Code/autosync/inventory"
+export UH_INVENTORY_REPO="$HOME/Code/autosync/libs/inventory"
 export UH_INVENTORY_DATA="$HOME/Code/autosync/_data"
 
 export CONTAINER_UID="$(id -u)"
