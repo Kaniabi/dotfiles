fda8b62e1da8e85c875bf7ba6b4297675f1a1f9b
Alexandre Andrade <kaniabi@gmail.com>
2025-06-02 15:09:55 +0000

    Adding plugin for vscode.

---
+++ plugins/vscode.zsh
@@ -0,0 +1,12 @@
+# VSCODE
+
+# vscode settings.json
+# {
+#   "terminal.integrated.defaultProfile.linux": "tmux-shell",
+#   "terminal.integrated.profiles.linux": {
+#     "tmux-shell": {
+#       "path": "tmux",
+#       "args": ["new-session", "-A", "-s", "vscode:${workspaceFolder}"]
+#     }
+#   }
+# }
